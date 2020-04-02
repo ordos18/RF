53c789ba607c9fb187f809e8c32bb390c3c572a3
Maciek <ordos18@gmail.com>
2020-04-02 13:20:36 +0200

    Ex. 9

--- main.c
+++ main.c
@@ -1,19 +1,16 @@
 #include "avr_compiler.h"
-#include "led.h"
+#include "acc.h"
 #include "rf.h"
 #include <avr/sleep.h>
-
-static unsigned char ucMsgCtr=0;
 
 #define SEND_RATE_HZ 100
 
 ISR(RTC_OVF_vect) { 
-	rf_send_byte(ucMsgCtr++);
-	//led_toggle();
+	rf_send_byte(acc_read());
 }
 
 int main (void) {
-	led_init();
+	acc_init();
 	RTC.PER = 1000/SEND_RATE_HZ - 1;
 	RTC.CNT = 0;
 	RTC.CTRL = RTC_PRESCALER_DIV1_gc;
@@ -32,8 +29,8 @@ int main (void) {
 /*
 current consumption (V_PROG): 
 
-2 Hz	~30 uA (7333 h = 306 days on CR2032 battery)
-100 Hz	180 uA (1222 h = 51 days on CR2032 battery)
+2 Hz	~100 uA (2200 h = 92 days on CR2032 battery)
+100 Hz	430 uA (511 h 21 days on CR2032 battery)
 
 Typical capacity of CR2032 battery: 220 mAh
 */
