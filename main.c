02cd19f8662065d367b58c7d6657d3db8d7aea6c
Maciek <ordos18@gmail.com>
2020-04-02 07:42:45 +0200

    Ex. 4

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
+#include "avr_compiler.h"
 #include "led.h"
-#include "rf.h"
-#include "port.h"
+
+#define BOOT_BTN_bm (1<<3)
+
+ISR(PORTC_INT0_vect){ // interrupt service routine
+	led_toggle();
+};
 
 int main (void){
-	
 	led_init();
-	rf_init_as_tx();
-	while(1) {
-		static unsigned char ucMsgCtr=0;
-		rf_send_byte(ucMsgCtr++);
-		rf_wait_until_data_sent();
-		led_toggle();
-	}
+	PORTC.DIRCLR = BOOT_BTN_bm;
+	PORTC.PIN3CTRL = PORT_OPC_PULLUP_gc | PORT_ISC_FALLING_gc;
+	PORTC.INT0MASK = BOOT_BTN_bm;
+	PORTC.INTCTRL = PORT_INT0LVL_LO_gc;
+	PMIC.CTRL = PMIC_LOLVLEN_bm;
+	sei(); // global interrupts enabling
+	while(1) {}
 }
+/*
+current consumption (V_PROG): 
+	LED OFF:
+		1.37 mA
+	LED ON:
+		1.60 mA
+*/
